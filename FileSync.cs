a70885a5149de0c15ef4ebf0911cfab741709bd3
Laci <jlacix@gmail.com>
2024-06-28 18:38:06 +0200

    Init

---
+++ FileSync.cs
@@ -0,0 +1,80 @@
+﻿using System.Diagnostics;
+
+namespace FileSync
+{
+    public class FileSync
+    {
+        public string Source;
+        public string Destination;
+        public string Pattern;
+
+        private FileSystemWatcher? fsWatcher;
+
+        public FileSync(string source, string destination, string pattern = "*.*")
+        {
+            this.Source = source;
+            this.Destination = destination;
+            this.Pattern = pattern;
+        }
+
+        public void Start()
+        {
+            fsWatcher = new FileSystemWatcher
+            {
+                Path = Source,
+                Filter = Pattern,
+                //NotifyFilter = NotifyFilters.LastWrite,
+            };
+
+            Console.WriteLine("Watching changes in " + Source);
+
+            string[] files = Directory.GetFiles(Source, Pattern);
+            Console.WriteLine("Found " + files.Length + " file(s)");
+
+            foreach (string filepath in files)
+            {
+                fsWatcherEvent(fsWatcher, new FileSystemEventArgs(0, Source, Path.GetFileName(filepath)));
+            }
+
+            fsWatcher.Created += fsWatcherEvent;
+            fsWatcher.Changed += fsWatcherEvent;
+            fsWatcher.Deleted += fsWatcherEvent;
+            
+            fsWatcher.EnableRaisingEvents = true;
+        }
+
+        public void Stop()
+        {
+            if (fsWatcher != null)
+            {
+                fsWatcher.EnableRaisingEvents = false;
+                fsWatcher.Dispose();
+            }
+        }
+
+        private void fsWatcherEvent(object sender, FileSystemEventArgs e)
+        {
+            if (e.Name != null)
+            {
+                string source = e.FullPath;
+                string destination = Path.Combine(Destination, e.Name);
+
+                switch (e.ChangeType)
+                {
+                    case WatcherChangeTypes.Created:
+                    case WatcherChangeTypes.Changed:
+                        if (e.ChangeType == 0)
+                        {
+                            Console.WriteLine(source + " => " + destination);
+                        }
+                        else
+                        {
+                            Console.WriteLine(source + " (" + e.ChangeType + ") => " + destination);
+                        }
+                        File.Copy(source, destination, true);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace FileSync
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = args[0] ?? "";
+            string destination = args[1] ?? "";
+            string pattern = args[2] ?? "*.*";
+            
+            if (source.Length == 0 || destination.Length == 0)
+            {
+                Console.WriteLine("Usage: filesync <source> <destination> <pattern>");
+            }
+            FileSync sync = new FileSync(source, destination, pattern);
+            EventWaitHandle waitHandle = new ManualResetEvent(false);
+
+            sync.Start();
+
+            waitHandle.Reset();
+
+            Console.CancelKeyPress += delegate (object? sender, ConsoleCancelEventArgs e) {
+                e.Cancel = true;
+                waitHandle.Set();
+                Debug.WriteLine("WaitHandle set");
+            };
+
+            waitHandle.WaitOne();
+
+            Debug.WriteLine("Stopping sync...");
+            sync.Stop();
+        }
+    }
+}
